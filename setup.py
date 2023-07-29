4ece22e290b2dff83e7d30d5269096f1cb1cfc7b
John Sheehan <jennasyseng@gmail.com>
2023-07-29 00:20:12 -0700

    Added dynamic package name to setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='jsxtopy',
-    version='0.1.2',
+    version='0.1.3',
     description="Converts a JSX fragment to a Python function equivalent",
     license="MIT",
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=[
         'lxml',
         'pyperclip',
     ],
-    packages=['jsxtopy'],
+    packages=find_packages(include=["jsxtopy"]),
     entry_points={
         'console_scripts': [
             'jsxtopy=jsxtopy:main'
